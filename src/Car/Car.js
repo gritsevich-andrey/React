fa46dc4d919724ebd663163bc26b9ef61125db4b
gritsevich-andrey <79464428+gritsevich-andrey@users.noreply.github.com>
2021-07-06 19:34:26 +0300

    Add files via upload

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,28 +1,15 @@
 import React from 'react'
 
-// function car() {
-//   return (
-//     <div>This is car component</div>
-//   )
-// }
-
-// const car = () => {
-//   return (
-//     <div>This is car component</div>
-//   )
-// }
-
-// const car = () => (
-//   <div>
-//     This is car component
-//     <strong>test</strong>
-//   </div>
-// )
-
-export default (props) => (
-  <div>
-      <p>{props.name}</p>
-      <p>{props.year}</p>
-      {props.children}
+export default props => (
+  <div style={{
+    border: '1px solid #ccc',
+    marginBottom: '10px',
+    display: 'block',
+    padding: '10px'
+  }}>
+    <h3>Сar name: {props.name}</h3>
+    <p>Year: <strong>{props.year}</strong></p>
+    <input type="text" onChange={props.onChangeName} value={props.name} />
+    <button onClick={props.onDelete}>Delete</button>
   </div>
 )
