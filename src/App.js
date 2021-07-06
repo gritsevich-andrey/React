7b0be39f2421250dc7b78f61843ff4d773936172
gritsevich-andrey <79464428+gritsevich-andrey@users.noreply.github.com>
2021-07-06 19:34:01 +0300

    Add dinamic list

--- src/App.js
+++ src/App.js
@@ -3,25 +3,69 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
-    state = {
-cars: [
-    {name: 'Ford', year: '2017'},
-    {name: 'Mazda', year: '2018'},
-    {name: 'Opel', year: '2019'}
-],
-        pageTitle: 'React component'
-    }
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010}
+    ],
+    pageTitle: 'React components',
+    showCars: false
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center'
     }
-const car = this.state.cars;
+
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+          />
+        )
+      })
+    }
+
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
-        <Car name = {car[0].name} year = {car[0].year} />
-        <Car name = {car[1].name} year = {car[1].year} />
-        <Car name = {car[1].name} year = {car[1].year} />
+
+        <button
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
+
+        { cars }
       </div>
     );
   }
